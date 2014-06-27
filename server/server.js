871c262e12e33cb20bca4f3a6bfd697b8d921a88
ikmlab <g21589@yahoo.com.tw>
2014-06-27 13:26:54 +0800

    Alpha
    Alpha Test

---
+++ server/server.js
@@ -0,0 +1,43 @@
+var io = require('socket.io').listen(8080);
+var currentSlide = {
+	indexh: 0,
+	indexv: 0,
+	indexf: 0
+};
+
+console.log('Start server');
+
+io.sockets.on('connection', function (socket) {
+	
+	socket.on('message', function (msg) {
+		console.log(msg);
+		if (msg == 'audience_connect') {
+			socket.emit('audience_chs', currentSlide);
+			console.log('213132');
+		}
+	});
+	
+	// 講者ready
+	socket.on('speaker_ready', function (e) {
+		console.log(e);
+		currentSlide = e;
+		socket.broadcast.emit('audience_chs', e);
+	});
+	
+	// 講者切換投影片
+	socket.on('speaker_chs', function (e) {
+		console.log(e);
+		currentSlide = e;
+		socket.broadcast.emit('audience_chs', e);
+	});
+	
+	// mousemove事件
+	socket.on('mousemove', function (mousePos) {
+		socket.broadcast.emit('mousemove', mousePos);
+	});
+	
+	socket.on('disconnect', function () {
+		console.log('disconnect');
+	});
+	
+});
