db758576ab06cb8afddd8350f4b0280ab3afc1de
tp6m4fu6250071 <tp6m4fu6250071@yahoo.com.tw>
2014-07-15 14:46:17 +0800

    Update 07-11 Fri..markdown

--- server/server.js
+++ server/server.js
@@ -46,15 +46,15 @@ io.sockets.on('connection', function (socket) {
 		socket.broadcast.emit('click', mousePos);
 	});
 	
-	// 送出Audio錄音緩衝區事件
-	socket.on('audio', function (audioData) {
-		socket.broadcast.emit('audio', audioData);
-	});
-	
 	socket.on('disconnect', function () {
 		console.log('Client disconnect');
 		io.emit('online_counter', --onlineCounter);
 		console.log("Online: " + onlineCounter);
+	});
+	
+	socket.on( 'QA_ask', function( questions)
+	{
+		socket.broadcast.emit('Ask_msg', questions);
 	});
 	
 });
